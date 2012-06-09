cf73f90965130c7e880ce5a7693c9d9ebae5d4b6
Bradley Matusiak <bmatusiak@gmail.com>
2012-06-09 03:06:15 -0400

    Updates

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var NoobHTTP = require('NoobHTTP');
 var PORT = 8003;
 
+
+
+var vork = require("./mvc/vork")();
      
 NoobHTTP.createServer({home: './webroot/',
             port : Number(process.env.PORT || PORT),
             on404 : function(req, res){
-                        return false;//needs to return default 404 headers, if true then leave this function to decide on what happens         
+                        return vork.mvc(req, res);    
                     }});
